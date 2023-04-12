5312fa0a415e948eb2bb78819156ed8cd3f65864
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-12 22:10:40 +0200

    Adds routes and main structure

---
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Venue from "../Venue/Venue";
+import Home from "../Home/Home";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import NotFound from "../NotFound/NotFound";
+import Layout from "../Layout/Layout";
+
+export default function AppRoutes() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="venue" element={<Venue />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="register" element={<Venue />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
