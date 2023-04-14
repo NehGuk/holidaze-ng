a2db35e90b8ac7559ac3226299684f6325624b33
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-14 12:40:56 +0200

    Adds register form and API call

--- src/components/Register/RegisterManager.jsx
+++ src/components/Register/RegisterManager.jsx
@@ -2,74 +2,77 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useState } from "react";
-import RegisterSuccess from "./RegisterSuccess";
+import { Link } from "react-router-dom";
 
-const schema = yup
-  .object({
-    userName: yup.string().min(3, "Your user name should be at least 3 characters.").required("Please enter your user name"),
-    email: yup
-      .string()
-      .email("Please enter a valid email address")
-      .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid email address")
-      .required("Please enter a valid email address"),
-    password: yup.string().min(3, "The password should be at least 3 characters").required("Please enter a password"),
-  })
-  .required();
+const schema = yup.object().shape({
+  name: yup.string().required("Please enter your name"),
+  email: yup.string().email("Please enter a valid email address").required("Please enter your email address"),
+  password: yup.string().required("Please enter your password").min(8, "Password should be at least 8 characters"),
+});
 
 export default function RegisterManager() {
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
 
-  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function onSubmit(data) {
-    data.venueManager = true;
-    console.log("Submitting the form");
-    console.log("Add the admin true the form object");
-    console.log(data);
-    console.log("Send the data to the API");
-    reset();
-    setFormSubmitted(true);
-  }
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch("https://api.noroff.dev/api/v1/holidaze/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
 
-  if (formSubmitted === true) {
-    return (
-      <div>
-        <RegisterSuccess />
-      </div>
-    );
-  }
+      if (response.ok) {
+        console.log("Boaaaa, isso ae!!!!!!!");
+        console.log(result);
+        setSuccess(true);
+        setError(false);
+      }
+
+      if (!response.ok) {
+        console.log("Ops, nao deu certo!!!");
+        console.log(result.errors[0].message);
+        setError(true);
+        setErrorMessage(result.errors[0].message);
+        setSuccess(false);
+      }
+    } catch (error) {}
+  };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="userName" hidden>
-          User name
-        </label>
-        <input name="userName" {...register("userName")} placeholder="User name" />
-        <p>{errors.userName?.message}</p>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" {...register("name")} />
+      {errors.name && <p>{errors.name.message}</p>}
 
-        <label htmlFor="email" hidden>
-          Email
-        </label>
-        <input name="email" {...register("email")} placeholder="name@email.com" />
-        <p>{errors.email?.message}</p>
+      <label htmlFor="email">Email:</label>
+      <input type="email" {...register("email")} />
+      {errors.email && <p>{errors.email.message}</p>}
 
-        <label htmlFor="password" hidden>
-          Password
-        </label>
-        <input name="password" {...register("password")} placeholder="Password" />
-        <p>{errors.password?.message}</p>
+      <label htmlFor="password">Password:</label>
+      <input type="password" {...register("password")} />
+      {errors.password && <p>{errors.password.message}</p>}
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <button type="submit">Register</button>
+
+      {error && <p>{errorMessage}</p>}
+      {success && (
+        <p>
+          Registration successful! Click here to <Link to="/login">Login</Link>.
+        </p>
+      )}
+    </form>
   );
 }
