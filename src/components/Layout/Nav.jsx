5cb40abade15b37fd92050d39be18828aaff4c06
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-19 19:36:02 +0200

    Renders navbar conditionally after login

--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -1,9 +1,21 @@
-/* import NavLoggedOut from "./NavLoggedOut/NavLoggedOut"; */
-/* import NavTraveller from "./NavTraveller/NavTraveller"; */
+import NavLoggedOut from "./NavLoggedOut/NavLoggedOut";
+import NavTraveller from "./NavTraveller/NavTraveller";
 import NavVenueManager from "./NavVenueManager/NavVenueManager";
+import { useAuthUser } from "react-auth-kit";
 
 export default function Nav() {
-  /* return <NavLoggedOut />; */
+  const userInfo = useAuthUser();
+  console.log(userInfo());
+
+  if (userInfo().venueManager === true) {
+    return <NavVenueManager />;
+  }
+
+  if (userInfo().venueManager === false) {
+    return <NavTraveller />;
+  }
+
+  return <NavLoggedOut />;
   /* return <NavTraveller />; */
-  return <NavVenueManager />;
+  /*   return <NavVenueManager />; */
 }
