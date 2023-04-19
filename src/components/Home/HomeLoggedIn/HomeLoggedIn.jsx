c4cc63355eff2b0ad22e4abcb5c2aa11a326a0ba
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-19 21:21:09 +0200

    Improves signout functionality

--- src/components/Home/HomeLoggedIn/HomeLoggedIn.jsx
+++ src/components/Home/HomeLoggedIn/HomeLoggedIn.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useIsAuthenticated } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+
 export default function HomeLoggedIn() {
+  const isAuth = useIsAuthenticated();
+  const navigate = useNavigate();
+  console.log(isAuth());
+
+  useEffect(() => {
+    if (isAuth() === false) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Homeeee logged innnn</h1>
+      {isAuth() === false && <h2>You are logged out.</h2>}
+      {isAuth() === true && <h2>Logged innnn</h2>}
     </div>
   );
 }
--- src/components/Layout/NavTraveller/NavTraveller.jsx
+++ src/components/Layout/NavTraveller/NavTraveller.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 import { NavTravellerStyle } from "./NavTraveller.style";
+import { useSignOut } from "react-auth-kit";
 
 export default function NavTraveller() {
+  const signOut = useSignOut();
+
+  const handleSignOut = () => {
+    signOut();
+  };
   return (
     <NavTravellerStyle>
       <div>Logo Nav Travellerrrr</div>
       <div>
         <Link to="login">Link 1</Link>
         <Link to="register">Link 2</Link>
-        <button>Sing out</button>
+        <button onClick={handleSignOut}>Sing out</button>
         <p>name and avatar</p>
       </div>
     </NavTravellerStyle>
