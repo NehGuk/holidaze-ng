74e2d94cef2badd0da582e8edb3a7fd34b1ae648
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-20 20:39:18 +0200

    Sets up new NavTraveller grid

--- src/components/Layout/NavTraveller/NavTraveller.jsx
+++ src/components/Layout/NavTraveller/NavTraveller.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavTravellerStyle } from "./NavTraveller.style";
 import { useSignOut, useAuthUser } from "react-auth-kit";
@@ -5,20 +6,29 @@ import logo from "../../../assets/logo.png";
 import { LogoImg } from "./NavTraveller.style";
 import avatar from "../../../assets/avatar.png";
 import { AvatarImg } from "./NavTraveller.style";
+import { HamburgerMenu } from "./NavTraveller.style";
 
 export default function NavTraveller() {
   const signOut = useSignOut();
   const userInfo = useAuthUser();
-
   const handleSignOut = () => {
     signOut();
   };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    console.log("OPENING MODAL");
+    console.log(isModalOpen);
+    setIsModalOpen(true);
+  };
+
   return (
     <NavTravellerStyle>
       <div>
         <Link to="/homein">
           <LogoImg src={logo} />
-          Logo Nav Traveller:
+          Logo Nav Travellerrrr
         </Link>
       </div>
       <div>
@@ -27,6 +37,9 @@ export default function NavTraveller() {
         <button onClick={handleSignOut}>Sing out</button>
         <p>{userInfo().name}</p>
         <AvatarImg src={avatar} />
+        <button onClick={openModal}>
+          <HamburgerMenu />
+        </button>
       </div>
     </NavTravellerStyle>
   );
