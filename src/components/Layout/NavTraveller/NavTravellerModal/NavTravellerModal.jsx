39a8d6aa6825e9fdf3221399f5c47880dc6487bc
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-21 12:05:26 +0200

    Adds user info to NavTravellerModal

--- src/components/Layout/NavTraveller/NavTravellerModal/NavTravellerModal.jsx
+++ src/components/Layout/NavTraveller/NavTravellerModal/NavTravellerModal.jsx
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
-import { ModalContainer, ModalContent } from "./NavTravellerModal.style";
+import { ModalBackground, ModalContainer, ModalContent } from "./NavTravellerModal.style";
 import { useNavigate } from "react-router-dom";
 import { ModalCloseIcon } from "./NavTravellerModal.style";
-import { useSignOut } from "react-auth-kit";
+import { useSignOut, useAuthUser } from "react-auth-kit";
+import avatar from "../../../../assets/avatar.png";
+import { AvatarImg } from "./NavTravellerModal.style";
 
 export default function NavTravellerModal({ showModal, setShowModal }) {
   console.log(showModal);
@@ -21,22 +23,29 @@ export default function NavTravellerModal({ showModal, setShowModal }) {
     signOut();
   };
 
+  const userInfo = useAuthUser();
+
   return (
     <div>
       {/* {goToLogin && <Navigate to="/login" />} */}
       {showModal ? (
-        <ModalContainer>
-          <ModalCloseIcon aria-label="Close modal" onClick={() => setShowModal((prev) => !prev)} />
-          <ModalContent>
-            <p>Avatar</p>
-            <p>Name</p>
-            <p>Account type: yyyy</p>
-            <button onClick={handleLoginButton}>Home</button>
-            <button onClick={handleLoginButton}>Profile</button>
-            <button onClick={handleLoginButton}>My bookings</button>
-            <button onClick={handleSignOut}>Sing out</button>
-          </ModalContent>
-        </ModalContainer>
+        <ModalBackground>
+          <ModalContainer>
+            <ModalCloseIcon aria-label="Close modal" onClick={() => setShowModal((prev) => !prev)} />
+            <ModalContent>
+              <AvatarImg src={avatar}></AvatarImg>
+
+              <p>User name: {userInfo().name}</p>
+              {userInfo().venueManager && <p>Account type: Venue manager</p>}
+              {!userInfo().venueManager && <p>Account type: Traveller</p>}
+
+              <button onClick={handleLoginButton}>Home</button>
+              <button onClick={handleLoginButton}>Profile</button>
+              <button onClick={handleLoginButton}>My bookings</button>
+              <button onClick={handleSignOut}>Sing out</button>
+            </ModalContent>
+          </ModalContainer>
+        </ModalBackground>
       ) : null}
     </div>
   );
--- src/components/Layout/NavTraveller/NavTravellerModal/NavTravellerModal.style.jsx
+++ src/components/Layout/NavTraveller/NavTravellerModal/NavTravellerModal.style.jsx
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 import { Close } from "@styled-icons/material/Close";
 
+export const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  /* left: 0; */
+  background: black;
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100vw;
+  height: 100vh;
+`;
+
 export const ModalContainer = styled.div`
   position: fixed;
   top: 0;
@@ -9,6 +21,7 @@ export const ModalContainer = styled.div`
   /* left: 0; */
 
   background: lightgrey;
+
   width: 50vw;
   height: 100vh;
 `;
@@ -18,6 +31,7 @@ export const ModalCloseIcon = styled(Close)`
   position: absolute;
   top: 5%;
   right: 2%;
+  z-index: 999;
 `;
 
 export const ModalContent = styled.div`
@@ -26,4 +40,9 @@ export const ModalContent = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+`;
+
+export const AvatarImg = styled.img`
+  max-width: 7em;
+  border-radius: 50%;
 `;
