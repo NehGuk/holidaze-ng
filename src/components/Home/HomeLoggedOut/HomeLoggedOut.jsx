1b471085f6a9c804afd19fd8b398eba9b5c02191
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-23 19:46:42 +0200

    Adds sub-components to HomeLoggedOut

---
+++ src/components/Home/HomeLoggedOut/HomeLoggedOut.jsx
@@ -0,0 +1,14 @@
+import Search from "../../Search/Search";
+import Venues from "../../Venues/Venues";
+import Welcome from "../Welcome";
+
+export default function HomeLoggedOut() {
+  return (
+    <div>
+      <h2>HomeLoggedOut Component</h2>
+      <Welcome />
+      <Search />
+      <Venues />
+    </div>
+  );
+}
