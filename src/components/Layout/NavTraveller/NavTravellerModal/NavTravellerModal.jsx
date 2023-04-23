4e1fa3226bd98103e7e886759dc8795285788eac
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-23 19:13:20 +0200

    Redirects to home after signing out

--- src/components/Layout/NavTraveller/NavTravellerModal/NavTravellerModal.jsx
+++ src/components/Layout/NavTraveller/NavTravellerModal/NavTravellerModal.jsx
@@ -27,6 +27,7 @@ export default function NavTravellerModal({ showModal, setShowModal }) {
 
   const signOut = useSignOut();
   const handleSignOut = () => {
+    navigate("/");
     signOut();
   };
 
