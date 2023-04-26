eb21d8c5fdb4022571e332640485754ed597fc6c
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-26 14:41:39 +0200

    Adds sample profile info

--- src/components/Profile/ProfileTraveller.jsx
+++ src/components/Profile/ProfileTraveller.jsx
@@ -1,7 +1,27 @@
+import { useAuthUser } from "react-auth-kit";
+import avatar from "../../assets/avatar.png";
+import { ProfileTravellerContainer } from "./ProfileTraveller.style";
+
 export default function ProfileTraveller() {
+  const userInfo = useAuthUser();
+  console.log(userInfo().venueManager);
+  console.log(userInfo().avatar);
   return (
-    <div>
-      <h2>Profile Traveller component</h2>
-    </div>
+    <ProfileTravellerContainer>
+      <div>
+        <h1>Profile</h1>
+        {userInfo().avatar === null && <img src={avatar} />}
+        {userInfo().avatar !== null && <img src={userInfo().avatar} />}
+        <button>Change avatar</button>
+
+        <p>Name: {userInfo().name}</p>
+        {!userInfo().venueManager && <p>Account type: Traveller</p>}
+        {userInfo().venueManager && <p>Account type: Venue manager</p>}
+
+        <p>Home</p>
+        <p>My bookings</p>
+        <p>Sign out</p>
+      </div>
+    </ProfileTravellerContainer>
   );
 }
