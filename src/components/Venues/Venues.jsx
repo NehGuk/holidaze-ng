07bb91f9ad48e55bd94e7c5c1c61860cea738b6b
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-27 19:40:17 +0200

    Adds venues map to HomeLoggedOut

--- src/components/Venues/Venues.jsx
+++ src/components/Venues/Venues.jsx
@@ -1,10 +1,48 @@
 import { Link } from "react-router-dom";
+import useApiVenues from "../../hooks/useAPIVenues";
+import { useEffect, useState } from "react";
+/* import VenueCard from "./VenueCard"; */
 
 export default function Venues() {
-  return (
-    <div>
-      <h2>Venue list</h2>
-      <Link to="/venue">Venue example</Link>
-    </div>
-  );
+  const { data, isLoading, isError } = useApiVenues("https://api.noroff.dev/api/v1/holidaze/venues?_owner=true&_bookings=true");
+  const [venuesList, setVenuesList] = useState([]);
+  useEffect(() => {
+    setVenuesList(data);
+  });
+
+  if (isLoading) {
+    return <div>LOADINNNGGG loading component</div>;
+  }
+  if (isError) {
+    return <div>ERRORRR component</div>;
+  }
+
+  if (data.length !== 0) {
+    return (
+      <div>
+        {isError && <p>SOME ERRORRRR</p>}
+
+        <h2>Venues component --for the venues list</h2>
+
+        <p>Map below</p>
+
+        {venuesList.map((item) => (
+          <div key={item.id}>
+            <div>
+              <div>
+                <Link to={`/venue?id=${item.id}`}>
+                  <img src={item.media} />
+                </Link>
+              </div>
+              <div>
+                <h3>{item.name}</h3>
+                <p>$ {item.price}</p>
+                <p>Rating: {item.rating}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
