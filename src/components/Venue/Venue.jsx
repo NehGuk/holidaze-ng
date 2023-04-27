b193e70023b0574376b9aa9935b5954ea1053381
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-04-27 20:34:22 +0200

    Adds data to Venue

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -1,8 +1,29 @@
+import { Link, useParams } from "react-router-dom";
+import useApiVenues from "../../hooks/useAPIVenues";
+
 export default function Venue() {
+  const params = useParams();
+  console.log(params.id);
+
+  const { data, isLoading, isError } = useApiVenues(`https://api.noroff.dev/api/v1/holidaze/venues/${params.id}?_owner=true&_bookings=true`);
+  console.log(data);
+  console.log(isLoading);
+  console.log(isError);
+  console.log(data.name);
+  console.log(data.description);
+  console.log(data.bookings);
+
   return (
     <div>
-      <h1>Venue page</h1>
-      <p>Venue info here</p>
+      <h1>{data.name}</h1>
+      <img src={data.media} />
+      <p>{data.description}</p>
+      <p>${data.price}</p>
+      <p>Rating: {data.rating}</p>
+      <p>Availability</p>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Create an account</Link>
+      <Link to="/">Back</Link>
     </div>
   );
 }
