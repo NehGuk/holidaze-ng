b182adf0d001930e1fe01e7f404fbbc1ddfaf1c9
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-01 20:25:13 +0200

    Tests why component is mounting twice

--- src/components/Venues/VenuesVenueManager.jsx
+++ src/components/Venues/VenuesVenueManager.jsx
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
-
 import { useEffect, useState } from "react";
-
 import useApiVenuesVenueManager from "../../hooks/useAPIVenuesVenueManager";
-
 import { useAuthUser } from "react-auth-kit";
 import { VenueManagerMyVenuesList } from "./VenuesVenueManager.style";
 
 export default function VenuesVenueManager() {
+  console.log("MOUNTED TWICE?");
   const userInfo = useAuthUser();
-  console.log(userInfo().name);
 
-  const { data, isLoading, isError } = useApiVenuesVenueManager(`https://api.noroff.dev/api/v1/holidaze/profiles/johntest2/venues`);
+  const { data, isLoading, isError } = useApiVenuesVenueManager(`https://api.noroff.dev/api/v1/holidaze/profiles/${userInfo().name}/venues`);
 
   const [myVenuesList, setMyVenuesList] = useState([]);
 
   useEffect(() => {
-    setMyVenuesList(data);
-  });
+    if (data.length !== 0) {
+      setMyVenuesList(data);
+    }
+  }, [data]);
 
   if (isLoading) {
     return <div>LOADINNNGGG loading component</div>;
@@ -26,38 +25,36 @@ export default function VenuesVenueManager() {
     return <div>ERRORRR component</div>;
   }
 
-  if (data.length !== 0) {
-    return (
-      <div>
-        {isError && <p>SOME ERRORRRR</p>}
+  return (
+    <div>
+      {isError && <p>SOME ERRORRRR</p>}
 
-        <h2>VenuesVenueManager component --for my specific venues</h2>
+      <h2>VenuesVenueManager component --for my specific venues</h2>
 
-        <p>Map below</p>
+      <p>Map below</p>
 
-        <VenueManagerMyVenuesList>
-          {myVenuesList.map((item) => (
-            <div key={item.id}>
+      <VenueManagerMyVenuesList>
+        {myVenuesList.map((item) => (
+          <div key={item.id}>
+            <div>
               <div>
-                <div>
-                  <Link to={`/venue/${item.id}`}>
-                    <img src={item.media} />
-                  </Link>
-                </div>
-                <div>
-                  <Link to={`/venue/${item.id}`}>
-                    <h3>{item.name}</h3>
-                  </Link>
+                <Link to={`/venue/${item.id}`}>
+                  <img src={item.media} />
+                </Link>
+              </div>
+              <div>
+                <Link to={`/venue/${item.id}`}>
+                  <h3>{item.name}</h3>
+                </Link>
 
-                  <p>${item.price}</p>
-                  <p>Rating: {item.rating}</p>
-                  <p>Max guests: {item.maxGuests}</p>
-                </div>
+                <p>${item.price}</p>
+                <p>Rating: {item.rating}</p>
+                <p>Max guests: {item.maxGuests}</p>
               </div>
             </div>
-          ))}
-        </VenueManagerMyVenuesList>
-      </div>
-    );
-  }
+          </div>
+        ))}
+      </VenueManagerMyVenuesList>
+    </div>
+  );
 }
