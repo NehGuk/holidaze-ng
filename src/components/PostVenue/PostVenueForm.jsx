efd9acfc8a41961a792975a9004501163a5d18c7
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-01 20:48:16 +0200

    Adds sample style to form

--- src/components/PostVenue/PostVenueForm.jsx
+++ src/components/PostVenue/PostVenueForm.jsx
@@ -4,6 +4,7 @@ import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 import PostVenueAPICall from "./PostVenueAPICall";
+import { FormContainer, FormFields } from "./PostVenueForm.style";
 
 const schema = Yup.object().shape({
   name: Yup.string().required("Please enter a name").max(100, "No more than 20 characters"),
@@ -54,52 +55,58 @@ export default function PostVenueForm() {
 
   return (
     <div>
-      <h1>Add new venue</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Name:</label>
-        <input type="text" {...register("name")} />
-        {errors.name && <span>{errors.name.message}</span>}
+      <FormContainer>
+        <div>
+          <h1>Add new venue</h1>
+        </div>
+        <FormFields>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Name:</label>
+            <input type="text" {...register("name")} />
+            {errors.name && <span>{errors.name.message}</span>}
 
-        <label>Description:</label>
-        <input type="text" {...register("description")} />
-        {errors.description && <span>{errors.description.message}</span>}
+            <label>Description:</label>
+            <textarea type="text" {...register("description")} />
+            {errors.description && <span>{errors.description.message}</span>}
 
-        <label>Media:</label>
-        <input type="text" {...register("media")} />
-        {errors.media && <span>{errors.media.message}</span>}
+            <label>Media:</label>
+            <input type="text" {...register("media")} />
+            {errors.media && <span>{errors.media.message}</span>}
 
-        <label>Price:</label>
-        <input type="number" {...register("price")} />
-        {errors.price && <span>{errors.price.message}</span>}
+            <label>Price:</label>
+            <input type="number" {...register("price")} />
+            {errors.price && <span>{errors.price.message}</span>}
 
-        <label>Max Guests:</label>
-        <input type="number" {...register("maxGuests")} />
-        {errors.maxGuests && <span>{errors.maxGuests.message}</span>}
+            <label>Max Guests:</label>
+            <input type="number" {...register("maxGuests")} />
+            {errors.maxGuests && <span>{errors.maxGuests.message}</span>}
 
-        <label htmlFor="pets">Pets</label>
-        <input type="checkbox" {...register("meta.pets")} />
+            <label htmlFor="pets">Pets</label>
+            <input type="checkbox" {...register("meta.pets")} />
 
-        <label htmlFor="breakfast">Breakfast</label>
-        <input type="checkbox" {...register("meta.breakfast")} />
+            <label htmlFor="breakfast">Breakfast</label>
+            <input type="checkbox" {...register("meta.breakfast")} />
 
-        <label htmlFor="breakfast">Wifi</label>
-        <input type="checkbox" {...register("meta.wifi")} />
+            <label htmlFor="breakfast">Wifi</label>
+            <input type="checkbox" {...register("meta.wifi")} />
 
-        <label htmlFor="parking">Parking</label>
-        <input type="checkbox" {...register("meta.parking")} />
+            <label htmlFor="parking">Parking</label>
+            <input type="checkbox" {...register("meta.parking")} />
 
-        <label htmlFor="address">Address</label>
-        <input type="text" {...register("location.address")} />
+            <label htmlFor="address">Address</label>
+            <input type="text" {...register("location.address")} />
 
-        <label htmlFor="city">City</label>
-        <input type="text" {...register("location.city")} />
+            <label htmlFor="city">City</label>
+            <input type="text" {...register("location.city")} />
 
-        <label htmlFor="country">Country</label>
-        <input type="text" {...register("location.country")} />
+            <label htmlFor="country">Country</label>
+            <input type="text" {...register("location.country")} />
 
-        <button type="submit">Submit</button>
-      </form>
-      <div>{formSubmitted && <PostVenueAPICall formData={formData} formSubmitted={formSubmitted} />}</div>
+            <button type="submit">Submit</button>
+          </form>
+        </FormFields>
+        <div>{formSubmitted && <PostVenueAPICall formData={formData} formSubmitted={formSubmitted} />}</div>
+      </FormContainer>
     </div>
   );
 }
