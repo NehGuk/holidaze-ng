e21f18aec8b7b680cb5736d3c391cf7f460161e8
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-04 14:06:04 +0200

    Adds shared.js for API endpoints

---
+++ src/shared/shared.js
@@ -0,0 +1,15 @@
+export default function api_endpoints(name, venueID) {
+  const object = {
+    postRegister: `https://api.noroff.dev/api/v1/holidaze/auth/register`,
+    postLogin: `https://api.noroff.dev/api/v1/holidaze/auth/login`,
+    putAvatar: `https://api.noroff.dev/api/v1/holidaze/profiles/${name}/media`,
+    getVenues: `https://api.noroff.dev/api/v1/holidaze/venues`,
+    getVenue: `https://api.noroff.dev/api/v1/holidaze/venues/${venueID}?_owner=true&_bookings=true`,
+    getProfile: `https://api.noroff.dev/api/v1/holidaze/profiles/${name}`,
+    postVenue: `https://api.noroff.dev/api/v1/holidaze/venues`,
+    putVenue: `https://api.noroff.dev/api/v1/holidaze/venues/${venueID}`,
+    testar2: "testando2",
+  };
+
+  return object;
+}
