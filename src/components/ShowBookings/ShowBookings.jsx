3783a499d61d743be0d33b59d7707ca839d03ed3
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-06 21:29:13 +0200

    Adds calendar showing booked dates logged out

--- src/components/ShowBookings/ShowBookings.jsx
+++ src/components/ShowBookings/ShowBookings.jsx
@@ -1,14 +1,39 @@
 import PropTypes from "prop-types";
+import ReactDatePicker from "react-datepicker";
+import { useState, useEffect } from "react";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function ShowBookings({ venueBookings }) {
-  console.log("MOUNTS SHOW BOOKINGS COMPONENT");
-  console.log(venueBookings);
-  console.log(venueBookings[0].dateFrom);
-  console.log(venueBookings[0].dateTo);
+  const [bookingsArray, setBookingsArray] = useState([]);
+
+  useEffect(() => {
+    const getBookedDates = () => {
+      const oneDayMs = 24 * 60 * 60 * 1000;
+      const newBookingsArray = venueBookings.map((booking) => ({
+        start: new Date(booking.dateFrom).getTime() - oneDayMs,
+        end: new Date(booking.dateTo).getTime() - oneDayMs,
+      }));
+      setBookingsArray(newBookingsArray);
+    };
+    getBookedDates();
+  }, [venueBookings]);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
+  const handleChangeDate = (dates) => {
+    const [start, end] = dates;
+    /* console.log(value); */
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  /* console.log(bookingsArray); */
 
   return (
     <div>
       <h3>Show Bookings component</h3>
+      <ReactDatePicker selected={startDate} onChange={handleChangeDate} startDate={startDate} endDate={endDate} selectsRange excludeDateIntervals={bookingsArray} placeholderText="Select a date other than the interval from 5 days ago to 5 days in the future" inline />
     </div>
   );
 }
