798817df38dc3426559c41528ba15901e2a7c7b9
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-07 22:30:58 +0200

    Sets up MyBookings API call

--- src/components/MyBookings/MyBookings.jsx
+++ src/components/MyBookings/MyBookings.jsx
@@ -1,16 +1,40 @@
 import useApi from "../../hooks/useAPI";
 import api_endpoints from "../../shared/shared";
+import { useAuthUser } from "react-auth-kit";
+import Loading from "../Loading/Loading";
 
 export default function MyBookings() {
-  useApi(api_endpoints());
+  const userInfo = useAuthUser();
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const { data, isLoading, isError, isSuccess } = useApi(api_endpoints(userInfo().name).getProfile, options);
+  console.log(data);
 
   return (
     <div>
       <h2>My bookings</h2>
-      <p>Show next bookings here made by traveller</p>
-      <p>Booking a</p>
-      <p>Booking b</p>
-      <p>Booking c</p>
+      {isLoading && <Loading />}
+      {isError && <p>An error has occurred</p>}
+      {isSuccess && (
+        <div>
+          {data.bookings.length === 0 && <p>No bookings yet</p>}
+          {data.bookings.length > 0 && (
+            <div>
+              <p>List of bookings below</p>
+              <p>Booking a</p>
+              <p>Booking b</p>
+              <p>Booking c</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
