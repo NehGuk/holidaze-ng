b3de4657e883a5d401fece138066f4f170482c71
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-07 23:02:36 +0200

    Adds API call to BookingTraveller

--- src/components/Booking/BookingTraveller.jsx
+++ src/components/Booking/BookingTraveller.jsx
@@ -1,9 +1,25 @@
 import { useParams } from "react-router-dom";
+import useApi from "../../hooks/useAPI";
+import api_endpoints from "../../shared/shared";
 
 export default function BookingTraveller() {
   console.log("MOUNTING BOOKING TRAVELLER COMPONENT");
   const { id } = useParams();
   console.log(id);
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const { data, isLoading, isError, isSuccess } = useApi(api_endpoints(null, null, id).getBooking, options);
+  console.log(data);
+  console.log(isLoading);
+  console.log(isError);
+  console.log(isSuccess);
 
   return (
     <div>
