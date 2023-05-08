72a29e84dad7cd38a9ea932880785c8f6b485f34
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-08 09:03:53 +0200

    Adds data to MyBookings

--- src/components/Booking/BookingTraveller.jsx
+++ src/components/Booking/BookingTraveller.jsx
@@ -1,6 +1,11 @@
 import { useParams } from "react-router-dom";
 import useApi from "../../hooks/useAPI";
 import api_endpoints from "../../shared/shared";
+import { BookingTravellerContainer } from "./BookingTraveller.style";
+import Loading from "../Loading/Loading";
+import estimatePrice from "../../utilities/estiamatePrice";
+import formatDate from "../../utilities/formatDate";
+import { Link } from "react-router-dom";
 
 export default function BookingTraveller() {
   console.log("MOUNTING BOOKING TRAVELLER COMPONENT");
@@ -20,10 +25,38 @@ export default function BookingTraveller() {
   console.log(isLoading);
   console.log(isError);
   console.log(isSuccess);
+  const { id: bid, dateFrom, dateTo, guests, created, venue } = data;
 
   return (
     <div>
-      <h2>Booking travellerrr </h2>
+      {isError && <p>An error has occurred</p>}
+      {isLoading && <Loading />}
+      {isSuccess && (
+        <BookingTravellerContainer>
+          <h2>Booking travellerrr </h2>
+          <img src={venue.media[0]} />
+          <p>Check-in: {formatDate(dateFrom)}</p>
+          <p>Check-out: {formatDate(dateTo)}</p>
+          <p>Number of guests: {guests}</p>
+          <p>Estimated cost: ${estimatePrice(dateFrom, dateTo, venue.price)}</p>
+          <p>Booked on {formatDate(created)}</p>
+          <p>Booking ID: {bid}</p>
+
+          <h3>Venue</h3>
+          {(venue.location.address === "" || venue.location.address === "Unknown") && <p>For directions, please contact the owner.</p>}
+          <p>Address: {venue.location.address}</p>
+          <p>City: {venue.location.city}</p>
+          <p>Country: {venue.location.country}</p>
+          <p>Price per night: ${venue.price}</p>
+
+          {venue.meta.wifi && <p>wifi</p>}
+          {venue.meta.parking && <p>parking</p>}
+          {venue.meta.breakfast && <p>breakfast</p>}
+          {venue.meta.pets && <p>pets allowed</p>}
+
+          <Link to="/my-bookings">Back to My Bookings</Link>
+        </BookingTravellerContainer>
+      )}
     </div>
   );
 }
