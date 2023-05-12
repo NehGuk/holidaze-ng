e643a32190b9ab57db0f5213bf3e7fe98e6428ff
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-12 17:12:47 +0200

    Adds booking history info

--- src/components/AllBookings/AllBookings.jsx
+++ src/components/AllBookings/AllBookings.jsx
@@ -12,7 +12,7 @@ export default function AllBookings() {
   const userInfo = useAuthUser();
 
   const { data, isLoading, isError, isSuccess } = useApi(api_endpoints(userInfo().name).getVenuesManager, createMethod("GET"));
-  /* console.log(data); */
+  console.log(data);
 
   const [totalBookings, setTotalBookings] = useState(0);
 
@@ -30,28 +30,62 @@ export default function AllBookings() {
 
   // CALCULATE TOTAL BOOKINGS END
 
+  // CREATE A STATE ARRAY WITH PAST BOOKINGS
+  /* const [pastBookings, setPastBookings] = useState([]);
+
+  useEffect(() => {
+    // Filter past bookings
+    const filteredBookings = data.flatMap((venue) => venue.bookings.filter((booking) => new Date(booking.dateTo) < new Date()));
+    console.log("Filtered bookings: ", filteredBookings);
+    setPastBookings(filteredBookings);
+  }, [data]);
+
+  console.log(pastBookings); */
+  // CREATE A STATE ARRAY WITH PAST BOOKINGS END
+
+  // CREATE A STATE ARRAY WITH UPCOMING BOOKINGS
+  /* const [upcomingBookings, setUpcomingBookings] = useState([]);
+
+  useEffect(() => {
+    // Filter past bookings
+    const filteredBookings = data.flatMap((venue) => venue.bookings.filter((booking) => new Date(booking.dateTo) > new Date()));
+    console.log("Filtered bookings: ", filteredBookings);
+    setUpcomingBookings(filteredBookings);
+  }, [data]);
+
+  console.log(upcomingBookings); */
+
+  // CREATE A STATE ARRAY WITH UPCOMING BOOKINGS END
+
+  const sortedChronos = data.sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom));
+  console.log(sortedChronos);
+
   return (
     <div>
       {isLoading && <Loading />}
       {isError && <p>An error has occurred</p>}
       {isSuccess && (
         <div>
-          <h1>All bookings</h1>
+          <h1>Bookings history</h1>
           <p>You have managed a total of {totalBookings} bookings.</p>
+
           {data.map((venue) => (
             <div key={venue.id}>
               {venue.bookings.length > 0 && (
                 <div>
-                  {venue.bookings.map((booking) => (
-                    <div key={booking.id}>
-                      <h2>{venue.name}</h2>
-                      <p>Booking ID: {booking.id}</p>
-                      <p>Check-in: {formatDate(booking.dateFrom)}</p>
-                      <p>Check-out: {formatDate(booking.dateTo)}</p>
-                      <p>Number of guests: {booking.guests}</p>
-                      <Link to={`/booking-venue-manager/${booking.id}`}>Details</Link>
-                    </div>
-                  ))}
+                  {venue.bookings
+                    .sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom))
+                    .map((booking) => (
+                      <div key={booking.id}>
+                        <img src={venue.media[0]} />
+                        <h2>{venue.name}</h2>
+                        <p>Booking ID: {booking.id}</p>
+                        <p>Check-in: {formatDate(booking.dateFrom)}</p>
+                        <p>Check-out: {formatDate(booking.dateTo)}</p>
+                        <p>Number of guests: {booking.guests}</p>
+                        <Link to={`/booking-venue-manager/${booking.id}`}>Details</Link>
+                      </div>
+                    ))}
                 </div>
               )}
             </div>
