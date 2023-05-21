3dd51fe2b24c680e2847868f2eb5471193e62e23
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-21 20:02:54 +0200

    Adds styles to booking behaviour

---
+++ src/components/BookVenue/BookVenue.style.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const BookingConfirmArea = styled.div`
+  margin-top: 2em;
+  background: ${(props) => props.theme.color.white};
+  max-width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+  padding: 1em;
+  /* display: grid; */
+  /* place-items: center; */
+  /* text-align: center; */
+
+  > p > span {
+    font-size: small;
+    font-weight: 600;
+  }
+
+  > div {
+    display: grid;
+    gap: 1em;
+    margin-top: 2em;
+  }
+`;
