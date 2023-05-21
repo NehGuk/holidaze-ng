684c9d46f4d05ca09e456280b125da986bfc047b
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-21 21:02:14 +0200

    Adds useEffect to scroll to top after confirmation

--- src/components/BookingConfirmed/BookingConfirmed.jsx
+++ src/components/BookingConfirmed/BookingConfirmed.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import { CTAArea, PageArea1Container, SLinkButton, Sh1Title } from "../styles/globalstyles";
 
 export default function BookingConfirmed() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <PageArea1Container>
       <Sh1Title>Nice one!</Sh1Title>
