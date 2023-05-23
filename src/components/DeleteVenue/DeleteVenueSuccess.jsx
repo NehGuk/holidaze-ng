97c6f4127743d6d207de9758bbdb216b34c6a25a
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-23 14:16:04 +0200

    Adds styles to DeleteVenueSuccess

--- src/components/DeleteVenue/DeleteVenueSuccess.jsx
+++ src/components/DeleteVenue/DeleteVenueSuccess.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom";
+/* import { Link } from "react-router-dom"; */
+import { CTAArea, PageArea1Container, SLinkButton, Sh1Title } from "../styles/globalstyles";
 
 export default function DeleteVenueSuccess() {
   console.log("delete successsss");
 
   return (
-    <div>
-      <h2>Venue Successfully deleted</h2>
-      <Link to="/post-new-venue">Create new venue</Link>
-      <Link to="/home">Go back home</Link>
-    </div>
+    <PageArea1Container>
+      <Sh1Title>Venue successfully deleted!</Sh1Title>
+      <CTAArea>
+        <SLinkButton $green to="/post-new-venue">
+          Create new venue
+        </SLinkButton>
+        <SLinkButton to="/home">Back to home</SLinkButton>
+      </CTAArea>
+    </PageArea1Container>
   );
 }
