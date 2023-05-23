5d799bb9feb9b0b040d7f80002a6d9d11413847d
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-23 13:48:42 +0200

    Adds styles to venue

--- src/components/Venue/VenueLoggedInVenueManager.jsx
+++ src/components/Venue/VenueLoggedInVenueManager.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import DeleteVenue from "../DeleteVenue/DeleteVenue";
 import { useState } from "react";
@@ -8,6 +8,9 @@ import Loading from "../Loading/Loading";
 import ShowBookingsListVenueManager from "../ShowBookings/ShowBookingsListVenueManager";
 
 import logo from "../../assets/logo.png";
+
+import { Area1, Area2, Area3, Area4, BreakfastIcon, ParkingIcon, PetsIcon, StarIcon, VenueContainer, WifiIcon } from "./VenueLoggedInVenueManager.style";
+import { CTAArea, SLinkButton, SSpanPrice, SSpanTitle, Sbutton, Sh1Title, Sh2CardTitle } from "../styles/globalstyles";
 
 export default function VenueLoggedInVenueManager() {
   const params = useParams();
@@ -40,49 +43,95 @@ export default function VenueLoggedInVenueManager() {
   };
 
   return (
-    <div>
+    <VenueContainer>
       {isError && <p>An error has occurred</p>}
       {isLoading && <Loading />}
       {isSuccess && (
         <div>
-          <h1>{data.name}</h1>
+          <Sh1Title>{data.name}</Sh1Title>
 
           {data.media.length === 0 && <img src={logo} />}
           {data.media.length > 0 && <img src={data.media[0]} />}
 
-          <p>{data.description}</p>
-          <p>${data.price}</p>
-          <p>Rating: {data.rating}</p>
-
-          <h3>Details</h3>
           <p>
-            This property accepts a maximum of <strong>{data.maxGuests}</strong> guests.
+            <SSpanTitle>Description: </SSpanTitle>
+            {data.description}
           </p>
-          {data.meta.wifi && <p>wifi</p>}
-          {data.meta.parking && <p>parking</p>}
-          {data.meta.breakfast && <p>breakfast</p>}
-          {data.meta.pets && <p>pets allowed</p>}
 
-          <h3>Location</h3>
-          {(data.location.address === "" || data.location.address === "Unknown") && <p>For directions, please contact the owner.</p>}
-          <p>{data.location.address}</p>
-          <p>{data.location.city}</p>
-          <p>{data.location.country}</p>
+          <Area1>
+            <p>
+              <SSpanPrice>${data.price}</SSpanPrice> per night
+            </p>
+          </Area1>
 
-          <h3>Admin</h3>
+          <Area2>
+            <Sh2CardTitle>Details</Sh2CardTitle>
 
-          <button onClick={handleUpdateVenueButton}>Update venue</button>
-          {!isDeleted && <button onClick={handleDeleteButton}>Delete venue</button>}
+            <p>
+              This property accepts a maximum of <strong>{data.maxGuests}</strong> guests.
+            </p>
 
-          <button onClick={handleShowBookingsButton}>{!showBookings ? "Show bookings for this venue" : "Hide bookings"}</button>
+            <div>
+              <>
+                <StarIcon /> <p>Rating: {data.rating}</p>
+              </>
+              {data.meta.wifi && (
+                <>
+                  <WifiIcon /> <p>Wifi </p>
+                </>
+              )}
+              {data.meta.parking && (
+                <>
+                  <ParkingIcon /> <p>Parking </p>
+                </>
+              )}
+              {data.meta.breakfast && (
+                <>
+                  <BreakfastIcon /> <p>Breakfast</p>
+                </>
+              )}
+              {data.meta.pets && (
+                <>
+                  <PetsIcon /> <p>Pets allowed</p>
+                </>
+              )}
+            </div>
+          </Area2>
 
-          {isDeleted && <DeleteVenue setIsDeleted={setIsDeleted} />}
+          <Area3>
+            <Sh2CardTitle>Location</Sh2CardTitle>
+            <p>
+              <SSpanTitle>Address: </SSpanTitle>
+              {data.location.address}
+            </p>
+            <p>
+              <SSpanTitle>City: </SSpanTitle>
+              {data.location.city}
+            </p>
+            <p>
+              <SSpanTitle>Country: </SSpanTitle>
+              {data.location.country}
+            </p>
+          </Area3>
 
-          {showBookings && <ShowBookingsListVenueManager venueBookings={venueBookings} />}
+          <Area4>
+            <CTAArea>
+              <Sh2CardTitle>Admin area</Sh2CardTitle>
+              <Sbutton onClick={handleUpdateVenueButton}>Update venue</Sbutton>
+              {!isDeleted && <Sbutton onClick={handleDeleteButton}>Delete venue</Sbutton>}
 
-          <Link to="/my-venues">Back to my venues</Link>
+              {isDeleted && <DeleteVenue setIsDeleted={setIsDeleted} />}
+              <Sbutton onClick={handleShowBookingsButton}>{!showBookings ? "Show bookings for this venue" : "Hide bookings"}</Sbutton>
+
+              {showBookings && <ShowBookingsListVenueManager venueBookings={venueBookings} />}
+
+              <SLinkButton $green to="/my-venues">
+                Back to my venues
+              </SLinkButton>
+            </CTAArea>
+          </Area4>
         </div>
       )}
-    </div>
+    </VenueContainer>
   );
 }
