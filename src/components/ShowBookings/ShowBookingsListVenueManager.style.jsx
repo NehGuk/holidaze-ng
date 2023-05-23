f3dba3a9a0743ae6317c40c7b3b8b4b01b5ddf06
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-23 15:01:48 +0200

    Adds styles to booking history on venue

---
+++ src/components/ShowBookings/ShowBookingsListVenueManager.style.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const BookingListGrid = styled.div`
+  padding: 1em 1em;
+  display: grid;
+
+  > div {
+    background: ${(props) => props.theme.color.shade};
+
+    border-radius: 10px;
+
+    padding: 2em 2em;
+    /* gap: 1em; */
+    max-width: 70%;
+    margin: 0 auto;
+    text-align: start;
+
+    > p {
+      color: ${(props) => props.theme.color.white};
+    }
+
+    > p > span {
+      font-size: small;
+      font-weight: 800;
+      text-transform: uppercase;
+    }
+  }
+`;
