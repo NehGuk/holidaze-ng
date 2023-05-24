c3da80b24005ac48ca03c6edbe4a3217a2b84078
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-24 13:20:04 +0200

    Adds spinner to CancelBooking form

--- src/components/CancelBooking/CancelBookingAPICall.jsx
+++ src/components/CancelBooking/CancelBookingAPICall.jsx
@@ -1,16 +1,16 @@
 import PropTypes from "prop-types";
 import api_endpoints from "../../shared/shared";
 import useApi from "../../hooks/useAPI";
-import Loading from "../Loading/Loading";
 import createMethod from "../../utilities/createMethod";
 import { Navigate } from "react-router-dom";
+import LoadingForm from "../Loading/LoadingForm";
 
 export default function CancelBookingAPICall({ bid }) {
   const { isLoading, isError } = useApi(api_endpoints(null, null, bid).deleteBooking, createMethod("DELETE"));
 
   return (
     <div>
-      {isLoading && <Loading />}
+      {isLoading && <LoadingForm />}
       {isError && (
         <div>
           <Navigate to="/booking-canceled-confirmed" />
