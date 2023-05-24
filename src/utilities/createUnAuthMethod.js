9940f80298974b42b0990fe6eee4ed56a2e52fae
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-05-24 22:30:01 +0200

    Adds JSDocs to utility functions

--- src/utilities/createUnAuthMethod.js
+++ src/utilities/createUnAuthMethod.js
@@ -1,3 +1,22 @@
+/**
+ * Creates unauthenticated fetch method
+ * @param {string} methodType Can be "GET", "POST", "PUT", "DELETE" etc...
+ * @param {object} content Has to be an object
+ * @returns {object} Returns the fetch method options to be passed in the API request
+ * @example
+ * / Create the options for an unauthenticated POST request
+ * const methodType = "POST"
+ * const avatarObject = {
+    media: "http://www.image-url.domain",
+  }
+ * const createUnAuthMethod("POST", avatarObject) = {
+  "method": "POST",
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "body": "{\"media\":\"http://www.image-url.domain\"}"
+} 
+ */
 export default function createUnAuthMethod(methodType, content) {
   const options = {
     method: methodType,
--- src/utilities/formatDate.js
+++ src/utilities/formatDate.js
@@ -1,3 +1,14 @@
+/**
+ * Formats dates
+ * @param {string} date Date as a Javascript string
+ * @returns {string} Formatted date
+ * @example
+ * ```js
+ * const date = "2023-05-24T22:00:00.000Z"
+ * const PrettyDate = formatDate(date)
+ * // The PrettyDate date will be the string "May 25, 2023"
+ * ```
+ */
 export default function formatDate(date) {
   return new Date(date).toLocaleDateString("en-US", {
     year: "numeric",
